ac914ba68fa802a39585d91dd2fe56aee53138d9
Akalisho <kubapustul7@gmail.com>
2023-10-03 09:53:59 +0200

    Zadania cz. 1

--- Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -3,16 +3,102 @@
 
 #include <iostream>
 
-int main()
+ //Napisz program któr wczyta liczbê od u¿ytkownika i j¹ wyœwietli na konsoli.
+void task1()
 {
-    //Napisz program któr wczyta liczbê od u¿ytkownika i j¹ wyœwietli na konsoli.
-
     std::cout << "Podaj numer:\n";
     int numberFromUser;
     std::cin >> numberFromUser;
-
     std::cout << "Podales: " << numberFromUser << "\n";
 
+}
+
+//Program obliczaj¹cy œredni¹ dwóch liczb
+
+void task2()
+{
+    int firstNumber, secondNumber;
+    std::cout << "Podaj pierwsza liczbe\n";
+    std::cin >> firstNumber;
+    std::cout << "Podaj druga liczbe\n";
+    std::cin >> secondNumber;
+    int average;
+    average = (firstNumber + secondNumber) / 2;
+    std::cout << "Srednia wynosi: " << average << "\n";
+}
+
+//Program obliczaj¹cy pole prostok¹ta.
+
+void task3()
+{
+    int firstSide, secondSide;
+    std::cout << "Podaj pierwszy bok\n";
+    std::cin >> firstSide;
+    std::cout << "Podaj drugi bok\n";
+    std::cin >> secondSide;
+    int area;
+    area = firstSide * secondSide;
+    std::cout << "Pole prostok¹ta wynosi: " << area << "\n";
+}
+
+//Program obliczaj¹cy objêtoœæ sto¿ka.
+
+void task4()
+{
+    double beam, height;
+    std::cout << "Podaj promieñ: \n";
+    std::cin >> beam;
+    std::cout << "Podaj wysokoœæ: \n";
+    std::cin >> height;
+    double volume;
+    volume = (height / 3) * 3.1415 * (beam * beam);
+    std::cout << "Objêtoœæ sto¿ka wynosi: " << volume << "\n";
+}
+
+//Program obliczaj¹cy pole ko³a.
+
+void task5()
+{
+    double beam;
+    std::cout << "Podaj promieñ: \n";
+    std::cin >> beam;
+    std::cout << "Pole ko³a to: \n" << (beam * beam) * 3.1415;
+}
+
+//Program obliczaj¹cy wartoœæ wyra¿enia a^2 + b^2.
+
+void task6()
+{
+    double SideA, SideB;
+    std::cout << "Podaj bok A: \n";
+    std::cin >> SideA;
+    std::cout << "Podaj bok B: \n";
+    std::cin >> SideB;
+    std::cout << "Wynik to: " << SideA * SideA + SideB * SideB;
+}
+
+//Program obliczaj¹cy pole trójk¹ta o podstawie b i wysokoœci h.
+
+void task7()
+{
+    double base_side, height;
+    std::cout << "Podaj bok podstawy: \n";
+    std::cin >> base_side;
+    std::cout << "Podaj wysokoœæ trójk¹ta: \n";
+    std::cin >> height;
+    std::cout <<
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "polish");
+    //task1();
+    //task2();
+    //task3();
+    //task4();
+    //task5();
+    //task6();
+    task7();
 }
 
 /*
