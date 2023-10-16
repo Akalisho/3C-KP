d034bcb9ed8c49aad6276865dfe0fe8e71b791bb
Akalisho <kubapustul7@gmail.com>
2023-10-16 16:01:38 +0200

    zadanie 2 z if/else

--- Programowanie/InstructionConsoleApplication/InstructionConsoleApplication.cpp
+++ Programowanie/InstructionConsoleApplication/InstructionConsoleApplication.cpp
@@ -30,9 +30,24 @@ void task1()
         std::cout << "Liczba jest równa 0.\n";
 }
 
+//Napisz program, który wyœwietli informacje, czy liczba jest parzysta czy nieparzysta.
+
+void task2()
+{
+    int number;
+    std::cout << "Podaj liczbê: \n";
+    std::cin >> number;
+
+    if (number % 2 == 0)
+        std::cout << "Liczba jest parzysta.";
+    else
+        std::cout << "Liczba jest nieparzysta.";
+}
+
 int main()
 {
     setlocale(LC_CTYPE, "polish");
-    task1();
+    //task1();
+    task2();
 }
     
