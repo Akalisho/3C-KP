2450a5c62a33324ae40e3bc38d1d0b123d40b28e
Akalisho <kubapustul7@gmail.com>
2023-12-08 14:22:34 +0100

    koniec lekcji, zrobione nast. zadania

--- Programowanie/LoopForConsoleApplication/LoopForConsoleApplication.cpp
+++ Programowanie/LoopForConsoleApplication/LoopForConsoleApplication.cpp
@@ -2,11 +2,6 @@
 using namespace std;
 
 /*
-//Program obliczający n!.
-//Program wyświetlający na ekranie silnie z liczb od 1 do 10 (np. 1!, 2!, 3!, 4! itd.)
-//Program wyświetlający na ekranie tabliczkę mnożenia od 1 do 9 (np. 1x1=1, 1x2=2, 1x3=3 itd.).
-//Program wyświetlający na ekranie wszystkie liczby podzielne przez 3 z zakresu od 1 do 100
-//Program obliczający sumę kwadratów liczb od 1 do 10
 //Program wyświetlający na ekranie ciąg Fibonacciego do 20 elementu (ciąg Fibonacciego to ciąg gdzie każdy element jest sumą dwóch poprzednich, np. 0, 1, 1, 2, 3, 5, 8, 13 itd.)
 //Program, kóry wyświetli poniższe cztery zwory:
 	****       54321        121212        122333
@@ -90,15 +85,75 @@ void task4()
 
 void task5()
 {
+	int i = 0;
+	for (int number = 1; number <= 10; number++)
+	{
+		std::cout << i << "\n";
+		i = number * number;
+	}
+	std::cout << i;
+}
+
+//Program obliczający n!.
+void task6()
+{
+	int number;
+	int sum = 1;
+	cout << "Podaj liczbe: \n";
+	cin >> number;
+
+	for (int i = 1; i != number + 1; i++)
+	{
+		sum *= i;
+	}
+
+	cout << sum;
+}
+
+//Program wyświetlający na ekranie silnie z liczb od 1 do 10 (np. 1!, 2!, 3!, 4! itd.)
+
+void task7()
+{
+	for (int i = 1; i <= 10; i++)
+	{
+		cout << i << "!, ";
+	}
+}
+
+//Program wyświetlający na ekranie tabliczkę mnożenia od 1 do 9 (np. 1x1=1, 1x2=2, 1x3=3 itd.).
+
+void task8()
+{
+	int number = 0;
+
+	for (int i = 1; i <= 10; number = i++)
+	{
+		cout << "1x" << number << "=" << number << ", ";
+	}
+}
+
+//Program wyświetlający na ekranie wszystkie liczby podzielne przez 3 z zakresu od 1 do 100
+
+void task9()
+{
+	int number = 3;
+	
+	for (int i = 3; number <= 100; number += i)
+	{
+		cout << number << ", ";
+	}
+}
+
+//Program obliczający sumę kwadratów liczb od 1 do 10
+
+void task10()
+{
 	int number;
 
-	for (; ; )
-	{
 
-	}
 }
 
 int main()
 {
-	task5();
+	task10();
 }
