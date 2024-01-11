a6157cfebb7ba2bf897b429839586fee1f656944
Akalisho <kubapustul7@gmail.com>
2024-01-11 08:11:20 +0100

    koniec lekcji

--- Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
+++ Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
@@ -40,7 +40,27 @@ void task1()
 
 void task2()
 {
+    //<LOWER_RANGE; UPPER_RANGE>, przy za³o¿eniu, ¿e UPPER_RANGE <= LOWER_RANGE
+    const unsigned short LOWER_RANGE = 5;
+    const unsigned short UPPER_RANGE = 7;
+    const unsigned ARRAY_SIZE = 3;
+    int numbers[ARRAY_SIZE] = {};
+    srand(time(0));
+    cout << "Wylosowane liczby:\n";
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        numbers[i] = rand() % (UPPER_RANGE - LOWER_RANGE + 1) + LOWER_RANGE;
+        cout << numbers[i] << ", ";
+    }
+    cout << "\n";
 
+    int sum = 0;
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        sum = sum + numbers[i];
+    }
+    double avg = sum * 1.0 / ARRAY_SIZE;
+    cout << "Œrednia wynosi: " << avg << "\n";
 }
 
 int main()
