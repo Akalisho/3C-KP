e42a6cb32b26d02095f72713a8ac9d3f47ac1024
Akalisho <kubapustul7@gmail.com>
2024-01-31 08:58:54 +0100

    koniec lekcji

--- Programowanie/FunctionConsoleApplication/FunctionConsoleApplication.cpp
+++ Programowanie/FunctionConsoleApplication/FunctionConsoleApplication.cpp
@@ -36,11 +36,33 @@ void task3(string name, int age)
         cout << name << " jest niepe³noletni/niepe³noletnia.\n";
 }
 
+//Napisz program, który przetestuje zachowanie siê przekazywanych danych przez parametr.
+void task4(int number)
+{
+    cout << "Zmienna number w funkcji task4: " << number << "\n";
+    number--;
+    cout << "Zmienna number w funkcji task4: " << number << "\n";
+}
+
+void task5(int &number)
+{
+    cout << "Zmienna number w funkcji task4: " << number << "\n";
+    number--;
+    cout << "Zmienna number w funkcji task4: " << number << "\n";
+}
+
+//Napisz program, który pobierze od u¿ytkownika liczbê.
+void task6_GetNumber(int &number)
+{
+    cout << "Podaj liczbê:\n";
+    cin >> number;
+}
+
 int main()
 {
     setlocale(LC_CTYPE, "polish");
 
-    task1();
+    /*task1();
     task1();
     task2("Witaj œwiecie!!!");
     task2("Ala ma kota");
@@ -48,5 +70,25 @@ int main()
     task2(text);
 
     task3("Jan", 35);
-    task3("Ala", 17);
+    task3("Ala", 17);*/
+
+    //int number = 5;
+    //cout << "Zmienna number w funkcji main: " << number << "\n";
+   // task4(number);
+    //cout << "Zmienna number w funkcji main: " << number << "\n";
+
+    // task4(9);
+    //const int NUMBER = 9;
+   // task4(NUMBER);
+
+    //cout << "Zmienna number w funkcji main: " << number << "\n";
+    //task5(number);
+    //cout << "Zmienna number w funkcji main: " << number << "\n";      
+
+    //task5(9) - //B£¥D - przez parametr mozna przekazaæ tylko zmienn¹
+    //task5(NUMBER) - //B£¥D - przez parametr mo¿na przekazaæ tylko zmienn¹
+
+    int numberFromUser = 5;
+    task6_GetNumber(numberFromUser);
+    cout << "U¿ytkownik poda³ liczbê: " << numberFromUser << "\n";
 }
