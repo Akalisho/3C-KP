1936b25f7cfdb7aadd8f137debc27100ee186c44
Uczen <Uczen@DESKTOP-6O2JQB3>
2024-09-19 09:46:22 +0200

    koniec lekcji

---
+++ Strukturalne/Programowanie/FirstStructConsoleApplication/Task2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+struct point
+{
+	double x;
+	double y;
+};
+
+void getPointValue(point &p)
+{
+	std::cout << "Podaj x: \n";
+	std::cin >> p.x;
+
+	std::cout << "Podaj y: \n";
+	std::cin >> p.y;
+}
+
+void task2()
+{
+	point firstPoint;
+
+	getPointValue(firstPoint);
+
+	double distance = sqrt(firstPoint.x * firstPoint.x + firstPoint.y * firstPoint.y);
+
+	std::cout << "Odleg³oœæ to: " << distance;
+}
