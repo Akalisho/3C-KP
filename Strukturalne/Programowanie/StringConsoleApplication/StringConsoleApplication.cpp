5e6192ca78e6d20c70236031ab4da7da4ae58d1b
Uczeń <Ucze@DESKTOP-RGP55N4>
2024-10-21 09:50:10 +0200

    koniec lekcji

---
+++ Strukturalne/Programowanie/StringConsoleApplication/StringConsoleApplication.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_CTYPE, "polish");
+    std::string text;
+
+    text = "Whats up";
+    std::cout << "Nasz tekst na początku: " << text << "\n";
+    std::cout << "Długość łańcucha to: " << text.length() << "\n";
+    std::cout << "Czy zmienna jest pusta: " << text.empty() << "\n";
+    text.append("my nigga!");
+    std::cout << "Nasz tekst na: " << text << "\n";
+}
