6fa41500605d70f966d876a10d1fd08cc2b45942
q <q>
2024-10-28 09:55:15 +0100

    kkk

--- Strukturalne/Programowanie/StringConsoleApplication/StringConsoleApplication.cpp
+++ Strukturalne/Programowanie/StringConsoleApplication/StringConsoleApplication.cpp
@@ -24,5 +24,14 @@ int main()
 
     text.insert(3, " nie");
     std::cout << "Zmiana tekstu na: " << text << "\n";
+
+    std::string text;
+
+    text = "Whats up";
+    std::cout << "Nasz tekst na pocz¹tku: " << text << "\n";
+    std::cout << "D³ugoœæ ³añcucha to: " << text.length() << "\n";
+    std::cout << "Czy zmienna jest pusta: " << text.empty() << "\n";
+    text.append("my nigga!");
+    std::cout << "Nasz tekst na: " << text << "\n";
 }
 
