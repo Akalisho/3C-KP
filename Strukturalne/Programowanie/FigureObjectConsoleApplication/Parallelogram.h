cdc7ba2419382dbc9daa4aa2d4d9edd37a9990f6
q <q>
2024-12-12 09:43:20 +0100

    koniec

--- Strukturalne/Programowanie/FigureObjectConsoleApplication/Parallelogram.h
+++ Strukturalne/Programowanie/FigureObjectConsoleApplication/Parallelogram.h
@@ -1,6 +1,27 @@
 #pragma once
-class Parallelogram
-{
+#include "Quadrangle.h"
 
+class Parallelogram : public Quadrangle
+{
+private:
+
+protected:
+
+public:
+	Parallelogram(double a, double b, double h, std::string n = "równoleg³obok") : Quadrangle(a, b, a, b, a, n)
+	{
+
+	}
+
+	void ShowInformation()
+	{
+		Quadrangle::ShowInformation();
+		std::cout << "Pole = " << GetArea() << "\n";
+	}
+
+	double GetArea()
+	{
+		return height * sideB;
+	}
 };
 
