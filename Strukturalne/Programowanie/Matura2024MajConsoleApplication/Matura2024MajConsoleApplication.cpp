311f9546fed57786d963b9c6c5c7416e61781ce7
q <q>
2025-04-24 12:19:28 +0200

    koniec lekcji

--- Strukturalne/Programowanie/Matura2024MajConsoleApplication/Matura2024MajConsoleApplication.cpp
+++ Strukturalne/Programowanie/Matura2024MajConsoleApplication/Matura2024MajConsoleApplication.cpp
@@ -2,6 +2,17 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <numeric>
+
+int GCD(int a, int b) {
+	while (b != 0)
+	{
+		int temp = b;
+		b = a % b;
+		a = temp;
+	}
+	return a;
+}
 
 int oddNumberAbbreviation(int n)
 {
@@ -40,7 +51,6 @@ int main()
 		std::vector<int> numbers;
 
 		int num;
-		int firstNumber = 0;
 
 		while (file >> num)
 			numbers.push_back(num);
@@ -65,7 +75,61 @@ int main()
 		}
 
 		std::cout << "Zadanie 3.2:\n";
+
+		int biggestOddAbbreviatedNumber = 0;
+		int notOddAbbreviatedNumbersCount = 0;
+
+		for (int num : numbers)
+		{
+			int result = oddNumberAbbreviation(num);
+
+			if (result == -1)
+			{
+				notOddAbbreviatedNumbersCount++;
+				if (num > biggestOddAbbreviatedNumber)
+					biggestOddAbbreviatedNumber = num;
+			}
+		}
+
+		std::cout << "Ilosc liczb z nieistniejacymi nieparzystymi skrotami: " << notOddAbbreviatedNumbersCount << "\n";
+		std::cout << "Najwieksza z liczb z nieistniejacymi nieparzystymi skrotami: " << biggestOddAbbreviatedNumber << "\n";
 	}
 	else
 		std::cout << "Plik nie poprawnie otwarty!!!\n";
+	
+	file.close();
+
+	std::ifstream file2("skrot2_przyklad.txt");
+
+	if (file2.is_open())
+	{
+		std::vector<int> numbers2; 
+
+		int num2;
+		 
+		while (file2 >> num2) 
+			numbers2.push_back(num2); 
+
+		std::cout << "\n\nZadanie 3.3:\n";
+
+		std::cout << "Odczytane liczby:\n";
+		for (int num2 : numbers2)
+		{
+			std::cout << num2 << ", "; 
+		}
+		std::cout << "\n";
+
+		std::cout << "Nieparzyste skroty liczb z NWD = 7:\n";
+
+		for (int num2 : numbers2) 
+		{
+			int result = oddNumberAbbreviation(num2); 
+
+			int greatestCommonDevider = GCD(num2, result);
+			if (greatestCommonDevider == 7)
+			{
+				std::cout << num2 << "\n";
+			}
+		}
+	}
 }
