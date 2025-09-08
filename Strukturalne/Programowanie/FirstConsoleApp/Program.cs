21e3130652fff8e0b78c5f7c245ade2107840d5b
Sala213 <Sala213@ezse.pl>
2025-09-08 13:17:04 +0200

    koniec pierwszej lekcji zapoznanie z 67

---
+++ Strukturalne/Programowanie/FirstConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Hello, Monkey!");
+Console.WriteLine("Hello, World!");
+string name = "6";
+string surname = "7";
+Console.WriteLine("Mangos " + name + "-" + surname + " Mustard");
+
