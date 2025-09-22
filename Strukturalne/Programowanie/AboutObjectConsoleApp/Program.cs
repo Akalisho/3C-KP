28dabbf51337d283ad03f525a7ff0191f646d7e6
Sala213 <Sala213@ezse.pl>
2025-09-22 13:14:01 +0200

    koniec lekcji dsdasdbjkasah

---
+++ Strukturalne/Programowanie/AboutObjectConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using AboutObjectConsoleApp;
+
+Point firstPoint = new Point();
+
+Point secondPoint = new Point(5, 9);
+
+firstPoint = new Point(18, -7);
+firstPoint = new (18, -7);
+
+firstPoint.Show();
+
+firstPoint.SetX(67);
+
