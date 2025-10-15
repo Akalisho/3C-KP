ad15d044134fb92b94447a619b4973c3cbf06e11
Sala213 <Sala213@ezse.pl>
2025-10-15 15:58:15 +0200

    koniec lekcji

---
+++ Strukturalne/Programowanie/PracticalTasksConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿
+using PracticalTasksConsoleApp.Styczeń2023_1;
+
+Task1 task1 = new Task1();
+task1.Task();
---
+++ Strukturalne/Programowanie/PracticalTasksConsoleApp/Styczeń2023_1/Task1.cs
@@ -0,0 +1,49 @@
+﻿
+namespace PracticalTasksConsoleApp.Styczeń2023_1;
+
+internal class Task1
+{
+    public void Task()
+    {
+        int a, b;
+        Console.WriteLine("Podaj a:");
+        a = int.Parse(Console.ReadLine());
+        if (a < 0)
+        {
+            a = a * (-1);
+        }
+        Console.WriteLine("Podaj b:");
+        b = int.Parse(Console.ReadLine());
+        if (b < 0)
+        {
+            b = b * (-1);
+        }
+        a = SzukajNWD(a, b);
+        Console.WriteLine("NWD podanych liczb: " + a);
+    }
+
+    /*
+    * nazwa funkcji: SzukajNWD
+    * opis funkcji: Wyszukuje NWD dwóch podanych liczb: a i b, po czym zwraca jego wartość
+    * parametry:  a - pierwsza podana liczba całkowita dodatnia
+    *             b - druga podana liczba całkowita dodatnia
+    * zwracany typ i opis: typ: int ,zmienna a (pierwsza podana liczba przez użytkownika, 
+    *                           zwrócona jako NWD wykonanej funkcji)
+    * autor: 01234567890
+    */
+    private int SzukajNWD(int a, int b)
+    {
+        while (a != b)
+        {
+            if (a > b)
+            {
+                a = a - b;
+            }
+            else
+            {
+                b = b - a;
+            }
+        }
+        return a;
+    }
+}
