99c7429d9c85c9f7051dadb46db8c1b16fa7e405
Sala213 <Sala213@ezse.pl>
2025-10-29 15:48:11 +0100

    koniec lekcji

---
+++ Strukturalne/Programowanie/PracticalTasksConsoleApp/Czerwiec2022_6/Task2.cs
@@ -0,0 +1,70 @@
+﻿
+namespace PracticalTasksConsoleApp.Czerwiec2022_6;
+
+internal class Osoba
+{
+    private int id = 0;
+    private string imie;
+    private static int licznik = 0;
+    public Osoba()
+    {
+        this.id = 0;
+        this.imie = "";
+        licznik++;
+    }
+
+    public Osoba(int id, string imie)
+    {
+        this.id = id;
+        this.imie = imie;
+        licznik++;
+    }
+
+    public Osoba(Osoba innaOsoba)
+    {
+        this.id = innaOsoba.id;
+        this.imie = innaOsoba.imie;
+        licznik++;
+    }
+
+    public void Wypisz(string argument)
+    {
+        if (string.IsNullOrEmpty(imie))
+        {
+            Console.WriteLine("brak danych");
+        }
+        else
+        {
+            Console.WriteLine($"Cześć {argument}, mam na imie {imie}");
+        }
+    }
+
+    public static void PokazLicznik()
+    {
+        Console.WriteLine($"Liczba utworzonych obiektow {licznik}");
+    }
+}
+
+/*
+* nazwa funkcji: Wypisz
+* opis funkcji: Wypisuje nazwę podaną w zmiennej argument i imie ,w przypadku gdy imie bedzie puste wypisze brak danych  
+* parametry:  argument - imie podane przez użytkownika do wyświetlenia w funkcji
+* zwracany typ i opis: typ: string
+* autor: 01234567890
+*/
+
+internal class Task2
+{
+    public void Task()
+    {
+        Osoba o1 = new Osoba();
+        Osoba o2 = new Osoba(1 , "Jan");
+        Osoba o3 = new Osoba(o2);
+
+        o1.Wypisz("Tomek");
+        o2.Wypisz("Kasia");
+        o3.Wypisz("Marek");
+
+        Osoba.PokazLicznik();
+    }
+}   
--- Strukturalne/Programowanie/PracticalTasksConsoleApp/Program.cs
+++ Strukturalne/Programowanie/PracticalTasksConsoleApp/Program.cs
@@ -1,5 +1,9 @@
 ﻿
-using PracticalTasksConsoleApp.Styczeń2023_1;
+//using PracticalTasksConsoleApp.Styczeń2023_1;
+using PracticalTasksConsoleApp.Czerwiec2022_6;
 
-Task1 task1 = new Task1();
-task1.Task();
+//PracticalTasksConsoleApp.Styczeń2023_1.Task1 task1 = new PracticalTasksConsoleApp.Styczeń2023_1.Task1();
+//task1.Task();
+
+PracticalTasksConsoleApp.Czerwiec2022_6.Task2 task2 = new PracticalTasksConsoleApp.Czerwiec2022_6.Task2();
+task2.Task();
